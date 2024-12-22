d648105094b61d82b19f781da136c7cb57876c43
1508334103 <1508334103@example.com>
2024-12-23 00:18:45 +0800

    Site updated: 2024-12-23 00:18:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/20/nice-day/"];function toRandomPost(){
+var posts=["2024/12/22/个人博客搭建流程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
