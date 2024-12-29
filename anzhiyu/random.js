b9e382958eaf183f7c083ff37d0db92e561e4ebc
1508334103 <1508334103@example.com>
2024-12-29 23:50:38 +0800

    Site updated: 2024-12-29 23:50:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/29/前端/AI/1、AIGC与生成式AI/","2024/12/29/前端/AI/2、LLM的演进之路/","2024/12/29/前端/AI/3、生成式大语言模型技术原理/","2024/12/29/前端/HTML/1、基本标签/"];function toRandomPost(){
+var posts=["2024/12/29/前端/AI/1、AIGC与生成式AI/","2024/12/29/前端/AI/2、LLM的演进之路/","2024/12/29/前端/HTML/1、基本标签/","2024/12/29/前端/AI/3、生成式大语言模型技术原理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
