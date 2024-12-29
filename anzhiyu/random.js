274260dad867f7af324a31e7cae43605c26adfda
1508334103 <1508334103@example.com>
2024-12-29 20:58:24 +0800

    Site updated: 2024-12-29 20:58:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/22/个人博客搭建流程/"];function toRandomPost(){
+var posts=["2024/12/29/AI/AIGC与生成式AI/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
